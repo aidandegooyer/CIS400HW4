aafebf7c22803497593c32e0677018c6db28d83f
aidanxi <aidanxiyt@gmail.com>
2024-02-01 14:40:48 -0500

    final version 2.0 fixed formatting, added more works, and fixed the plot to use log scale

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.utils import resample
 from keras.models import Sequential
 from keras.layers import Dense
@@ -21,13 +20,17 @@ from keras.optimizers import Adam
 # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.iloc.html
 # https://scikit-learn.org/stable/modules/generated/sklearn.preprocessing.StandardScaler.html
 # https://towardsdatascience.com/what-and-why-behind-fit-transform-vs-transform-in-scikit-learn-78f915cf96fe
+# https://matplotlib.org/3.5.3/api/_as_gen/matplotlib.pyplot.html
+# https://www.javatpoint.com/how-to-add-two-lists-in-python
+# https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
 #
 # Debugging: https://datascience.stackexchange.com/questions/67047/loss-being-outputed-as-nan-in-keras-rnn
 #            https://stackoverflow.com/questions/49135929/keras-binary-classification-sigmoid-activation-function
 #
 
+# calculates the value of q for an epoch
 def calculate_q(confusion_matrix, b1, b2):
-    # Extract true positives (TP), true negatives (TN), false positives (FP), and false negatives (FN)
+    # Extract false positives (FP) and false negatives (FN)
     FP = confusion_matrix[0, 1]
     FN = confusion_matrix[1, 0]
 
@@ -49,13 +52,14 @@ test_data = test_data.iloc[:, 2:]
 train_data.fillna(train_data.mean(), inplace=True)
 test_data.fillna(test_data.mean(), inplace=True)
 
+# set number of data points from each class
 b1 = 1000
 b2 = 3000
 
 class_0_train = train_data[train_data['label'] == 0]
 class_1_train = train_data[train_data['label'] == 1]
 
-# Sample 600 data points from one class and 200 from the other for training
+# Sample b1 data points from one class and b2 from the other
 train_class_0 = resample(class_0_train, n_samples=b1, random_state=42)
 train_class_1 = resample(class_1_train, n_samples=b2, random_state=42)
 train_data = pd.concat([train_class_0, train_class_1])
@@ -64,8 +68,10 @@ train_data = pd.concat([train_class_0, train_class_1])
 num_samples_class_0_relabel = int(0.05 * len(train_class_0))
 num_samples_class_1_relabel = int(0.05 * len(train_class_1))
 
-selected_indices_class_0_relabel = np.random.choice(train_class_0.index, size=num_samples_class_0_relabel, replace=False)
-selected_indices_class_1_relabel = np.random.choice(train_class_1.index, size=num_samples_class_1_relabel, replace=False)
+selected_indices_class_0_relabel = np.random.choice(train_class_0.index, size=num_samples_class_0_relabel,
+                                                    replace=False)
+selected_indices_class_1_relabel = np.random.choice(train_class_1.index, size=num_samples_class_1_relabel,
+                                                    replace=False)
 
 train_data.loc[selected_indices_class_0_relabel, 'label'] = 1
 train_data.loc[selected_indices_class_1_relabel, 'label'] = 0
@@ -96,6 +102,7 @@ X_test = scaler.transform(X_test)
 input_dim = X_train.shape[1]
 
 
+# function to train the model
 def train_model(X_train, y_train, X_test, y_test, b1, b2):
     input_dim = X_train.shape[1]
     model = Sequential()
@@ -104,6 +111,8 @@ def train_model(X_train, y_train, X_test, y_test, b1, b2):
     model.add(Dense(units=1, activation='sigmoid'))
     optimizer = Adam(clipvalue=0.5)
     model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+    # sets up an array to catch all the q values from each epoch
     q_train_per_epoch = []
     q_test_per_epoch = []
 
@@ -126,8 +135,6 @@ def train_model(X_train, y_train, X_test, y_test, b1, b2):
         q_train_per_epoch.append(q_train)
         q_test_per_epoch.append(q_test)
 
-        train_loss, train_accuracy = model.evaluate(X_train, y_train, verbose=0)
-        test_loss, test_accuracy = model.evaluate(X_test, y_test, verbose=0)
         print(
             f"Epoch {epoch + 1}: Train Loss = {train_loss:.4f}, Train Accuracy = {train_accuracy:.4f}, Test Loss = {test_loss:.4f}, Test Accuracy = {test_accuracy:.4f}")
 
@@ -137,19 +144,21 @@ def train_model(X_train, y_train, X_test, y_test, b1, b2):
 # Repeat experiment
 epochs = 50
 num_experiments = 10
+
 q_train_avg = np.zeros(epochs)
 q_test_avg = np.zeros(epochs)
 confusion_matrices_train = []
 confusion_matrices_test = []
 
-
 for i in range(num_experiments):
     history, q_train_per_epoch, q_test_per_epoch, trained_model = train_model(X_train, y_train, X_test, y_test, b1, b2)
 
+    # add the q values from each epoch to a total from all experiments (will be used for averaging later)
     q_train_avg = [a + b for a, b in zip(q_train_avg, q_train_per_epoch)]
     q_test_avg = [a + b for a, b in zip(q_test_avg, q_test_per_epoch)]
 
-    print(f"**********************************************EXPERIMENT NUMBER {i + 1} COMPLETE**********************************************")
+    print(
+        f"**********************************************EXPERIMENT NUMBER {i + 1} COMPLETE**********************************************")
 
     # Calculate confusion matrices
     y_pred_train = np.round(trained_model.predict(X_train)).astype(int)
@@ -164,20 +173,21 @@ for i in range(num_experiments):
     print(f"Confusion Matrix - Test Data (Experiment {i + 1}):")
     print(confusion_matrices_test[i])
 
+# averaging q values per epoch
 q_train_final = []
 for x in q_train_avg:
-    q_train_final.append(x/num_experiments)
+    q_train_final.append(x / num_experiments)
 q_test_final = []
 for x in q_test_avg:
-    q_test_final.append(x/num_experiments)
+    q_test_final.append(x / num_experiments)
 
-#PLOT GRAPH
+# plot the graph using matplotlib
 
 plt.figure(figsize=(10, 6))
-plt.plot(range(1, epochs + 1), q_train_final, label='Training Data')
-plt.plot(range(1, epochs + 1), q_test_final, label='Test Data')
-plt.xlabel('Experiment Number')
+plt.plot(np.log(range(1, epochs + 1)), q_train_final, label='Training Data')
+plt.plot(np.log(range(1, epochs + 1)), q_test_final, label='Test Data')
+plt.xlabel('log(Number of Weight Updates)')
 plt.ylabel('Average q')
-plt.title('Average q vs. Experiment Number')
+plt.title('Average q vs. log(Number of Weight Updates)')
 plt.legend()
 plt.show()
